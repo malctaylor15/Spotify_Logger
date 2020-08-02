8ee5add26b22315240c8734aa7f91239a8e07069
malctaylor15 <malcolmtaylor@Malcolms-MacBook-Air.local>
2020-08-02 11:29:10 -0400

    converting to prod tracking version only

---
+++ README.md
@@ -0,0 +1,13 @@
+## Spotify Logger  
+
+Repository to save spotify listening history to database 
+
+This will allow for future analysis of different genre's, listening trends etc 
+
+
+Spotify does not allow the user to see the entire listening history at once. 
+So we use spotify's API to save recently listened history to sqlite database. We can set a scheduler to check every 2 hours. 
+
+Once the data is captured, we can do analysis later on by tracking popular artists, genre trends, and potential lyrical trends. 
+This is repository simply allows for the tracking of the spotify data. 
+
---
+++ run_notebook.sh
@@ -0,0 +1,34 @@
+export path1="$(dirname "$0")"
+cd $path1
+echo "current working directory: "$PWD
+
+# File name set up
+DATE=`date +%m-%d-%Y`
+RN_FILENAME=Run_Notebooks_${DATE}.ipynb
+LOCATION=run_notebooks/
+RN_FILEPATH=$LOCATION$RN_FILENAME
+
+# Generic venv because requirements are minimal
+source /home/pi/venv/bin/activate
+
+
+echo "Will be saving new notebook to: "$RN_FILEPATH
+papermill Run_Notebooks.ipynb $RN_FILEPATH -p db_location data/listening_history.db
+# Testing 
+# papermill Run_Notebooks.ipynb $RN_FILEPATH -p db_location data/listening_history_qa.db 
+# 
+export papermill_exit_status=$?
+if [ $papermill_exit_status -eq 0 ]
+then
+  echo "removing "$RN_FILEPATH
+  rm $RN_FILEPATH
+fi
+
+
+
+deactivate
+exit 0
+
+# crontab -e
+# 45 9 * * * * bash scripts/run_notebooks.sh >> /home/pi/tickets_pull/run_notebook.log 2>&1
+
