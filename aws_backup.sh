4c70ebdf3dfd6cecafff608a92832dbe1f7474bd
malctaylor15 <malctaylor15@gmail.com>
2020-08-10 22:51:07 -0400

    Added aws backup

--- aws_backup.sh
+++ aws_backup.sh
@@ -4,4 +4,8 @@ echo "current working directory: "$PWD
 
 DATE=`date +%m-%d-%y`
 new_file_name=listening_history_${DATE}.db
-/home/pi/.local/bin/aws s3 cp data/listening_history.db s3://scraper-backup/listening_history/${new_file_name}
+/home/pi/.local/bin/aws s3 cp data/listening_history.db s3://scraper-backup/Listening_History/${new_file_name}
+
+#crontab -e
+# 35 21 * * * bash /home/pi/Loggers/Spotify_Logger/aws_backup.sh
+
