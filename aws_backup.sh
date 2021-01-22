954bb5101d096265367ebddb87f3a0646917f54a
malctaylor15 <malctaylor15@gmail.com>
2021-01-22 05:41:27 +0000

    artist and playlist updates

--- aws_backup.sh
+++ aws_backup.sh
@@ -2,9 +2,10 @@ export path1="$(dirname "$0")"
 cd $path1
 echo "current working directory: "$PWD
 
+export aws="/usr/local/bin/aws"
 DATE=`date +%m-%d-%y`
 new_file_name=listening_history_${DATE}.db
-aws s3 cp data/listening_history.db s3://do-mt-backups/Listening_History/${new_file_name}
+$aws s3 cp data/listening_history.db s3://do-mt-backups/Listening_History/${new_file_name}
 
 #crontab -e
 # 35 21 * * * bash /home/malcolm/Loggers/Spotify_Logger/aws_backup.sh
